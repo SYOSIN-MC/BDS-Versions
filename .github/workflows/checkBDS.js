6dbf438b9337d171b35d26893f3074710be04feb
kaito02020424 <104062884+kaito02020424@users.noreply.github.com>
2024-10-03 06:34:24 +0000

    fix: 無限失敗バグ

--- .github/workflows/checkBDS.js
+++ .github/workflows/checkBDS.js
@@ -1,18 +1,24 @@
 const exec = require('child_process').exec;
 const fs = require('fs');
 /**
- * bump: x.x.x.x
- */
+ * aaabbbcc refs/tags/1.xx.yy.zz
+*/
 const lastCommitMessage = process.argv[2];
-const lastVersion = lastCommitMessage.match(/bump: ([0-9.]+)/)?.[1];
+const lastVersions = lastCommitMessage.matchAll(/refs\/tags\/([0-9.]+)/g);
+/**
+ * @type string[]
+ */
+const knownVersions = [];
+for (const v of lastVersions) knownVersions.push(v[1]);
+console.log(knownVersions);
 
 (async () => {
     const data = await (await fetch("https://www.minecraft.net/en-us/download/server/bedrock")).text()
-    const winURL = data.match(/https:\/\/minecraft\.azureedge\.net\/bin-win\/bedrock-server-([0-9.]+)\.zip/)[0]
-    const linuxURL = data.match(/https:\/\/minecraft\.azureedge\.net\/bin-linux\/bedrock-server-([0-9.]+)\.zip/)[0]
+    const winURL = data.match(/https:\/\/www\.minecraft\.net\/bedrockdedicatedserver\/bin-win\/bedrock-server-([0-9.]+)\.zip/)[0]
+    const linuxURL = data.match(/https:\/\/www\.minecraft\.net\/bedrockdedicatedserver\/bin-linux\/bedrock-server-([0-9.]+)\.zip/)[0]
 
     const version = winURL.match(/bedrock-server-([0-9.]+)\.zip/)[1]
-    if (version === lastVersion) {
+    if (knownVersions.includes(version)) {
         console.log("skip")
         process.exit(0)
     }
